56b21551ea221e0a4334ecc997c0fe18ef728fd8
danblack101 <danblack101@gmail.com>
2013-11-14 14:02:35 +1300

    initial commit

---
+++ ContextSingletons/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContextSingletons.Models;
+using ContextSingletons.Models.Donkey;
+
+namespace ContextSingletons.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DonkeyViewer()
+        {
+            SmallAss smallAss = SmallAss.Instance;
+            return View(smallAss);
+        }
+    }
+}
---
+++ ContextSingletons/Models/Donkey/SmallAss.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web;
+
+namespace ContextSingletons.Models.Donkey
+{
+    public abstract class DonkeyBase
+    {
+        protected readonly int Age;
+        protected readonly int NumberOfLegs;
+
+        protected DonkeyBase(int age, int numberOfLegs)
+        {
+            Age = age;
+            NumberOfLegs = numberOfLegs;
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+                throw new Exception("This aint real no donkey");
+        }
+
+        public string Name
+        {
+            get { return HttpContext.Current.User.Identity.Name; }
+        }
+
+        public abstract int CarryingCapacity();
+    }
+
+    public class SmallAss : DonkeyBase
+    {
+        private const int MAX_CARRYING_CAPACITY = 30;
+        private static SmallAss _instance;
+
+        private SmallAss(int age, int numberOfLegs) : base(age, numberOfLegs)
+        {
+        }
+
+        public static SmallAss Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SmallAss(5, 4);
+                }
+                return _instance;
+            }
+        }
+
+        public override int CarryingCapacity()
+        {
+            if (Age < 5)
+                return 0;
+            return (5 - NumberOfLegs)*MAX_CARRYING_CAPACITY;
+        }
+    }
+}
